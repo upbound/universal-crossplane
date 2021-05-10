79a083a87a41ff1e55c8c1d4850931a81a5ab603
Nic Cope <negz@rk0n.org>
2021-05-10 20:16:50 +0000

    Trim off manually added headers, let addlicense write them
    This makes us consistent on the (unforuntate) // comment syntax for license
    headers, but is otherwise a no-op.
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- internal/controllers/billing/aws/aws.go
+++ internal/controllers/billing/aws/aws.go
@@ -1,18 +1,16 @@
-/*
-Copyright 2021 Upbound Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2021 Upbound Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package aws
 
@@ -23,11 +21,12 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/marketplacemetering"
-	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
 )
 
 // These constants are given by AWS Marketplace.
